1354fe64fc3f24c5d88cc4534a780ac4fa009624
David Karlaš <david.karlas@xamarin.com>
2015-11-25 19:48:57 +0100

    crash

--- Program.cs
+++ Program.cs
@@ -5,17 +5,6 @@ namespace TestApp
 {
 	class MainClass
 	{
-		public static string TestProp
-		{
-			get
-			{
-				var sw = Stopwatch.StartNew();
-				while (sw.ElapsedMilliseconds < 5000) {
-				}
-				return "5 seconds passed";
-			}
-		}
-
 		public static void Main(string[] args)
 		{
 			Debugger.Break();
